89f28574cc41bad9a763cbb3bc4ae63252973c9e
David Vo <user@davd-book.local>
2024-07-31 22:04:54 -0700

    added pseudocode for assignment

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def test_z_score_function():
 # YOUR CODE GOES BELOW THIS BOX.                      #
 #                                                     #
 # Complete the following z_score function.            #
-# You may call the functions above,	              #
+# You may call the functions above,	                  #
 #   but do not define any others (except for testing) #
 # You may use arithmetic operators                    #
 #  (i.e., +, -, *, **, /) but not Python Boolean      #
@@ -96,17 +96,47 @@ def test_z_score_function():
 # participated in the group assignment work           #
 #######################################################
 
+#TODO:
 def z_score(x, mu, sigma):
     """
-    x is the population item
+
+    z = ((x-mu)/sigma)
+
+    x is the population array element
     mu is the population mean
     sigma is the population standard deviation
     
     Returns the z-score of x
     """
+
+    # 1. Review the starter code shown below. Your task is to complete the function at the bottom of the code so that it emulates the given formula.
+    # 2. Review and refine your existing plan to allocate the work among your team members. For example, you might decide that one student will write the first draft of the algorithm, another will check it and write tests, and the third can implement the algorithm in Python. 
+    # 3. Draw up your testing plans (your test cases and method for obtaining expected outputs). You may use Sheets like in Exercise 3, use other software instead, or do it by hand. 
+    # 4. Write an algorithm to solve the problem and create either pseudocode or a flowchart to represent it.
+    # 5. Complete the program so that the function produces the correct output using the given parameters. Do not change the function name or parameters, or any of the other functions in the code. It is OK if you want to add additional functions, such as functions that help automate your testing processes. You can do this work directly in the Snippet, or you can use another IDE of you want.
+    # 6. Test your code and document the test results. Your documented tests (in your report) should include at least 2 cases that aren't already in the grader's test_z_score_function() in the starter code.
+    # 7. Adjust the script as needed until the tests produce the expected output.
     
     # Participating group member names go in this comment
-    
+    # Kareem Mckenzie
+    # Daniel Aguilar
+    # Nghia Vo
+
+
+
     # Your code goes between this comment and the return
 
-    return
+    return ((x-mu)/sigma)
+
+
+'''
+part 1: calculate population mean
+    - calculated through mean function (pass it the entire array)
+    - save this value to a variable
+part 2: calculate population's std deviation
+    - pass the population array and the mean from part 1
+    - save this to a varaible
+part 3: caclculate zScore of item
+    - while looping through an array, pass the current element to the z_score function, the varaible saved in part 1, and the variable saved in part 2
+    - print/return the result of each z score
+'''
